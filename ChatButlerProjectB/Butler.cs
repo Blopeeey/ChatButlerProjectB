46eb247b6ef8dd7c8bfd4dc21c63c4be67aaa5a7
blopeeey <basweidmann@hotmail.com>
2021-03-22 17:06:57 +0100

    Added basic menu

--- ChatButlerProjectB/Butler.cs
+++ ChatButlerProjectB/Butler.cs
@@ -9,7 +9,14 @@
 
         public string Greet()
         {
-            return "Hi";
+            return "Waarde heer, mevrouw. \n\nWelkom bij restuarant La Mouette. \n\nWaar kan ik u mee van dienst zijn?\n";
+        }
+
+        public string ShowComponents()
+        {
+            return "1: Een review bekijken van eerdere gasten\n" +
+                   "2: Een reservering plaatsen\n" +
+                   "3: Een account maken\n";
         }
     }
 }
--- ChatButlerProjectB/Program.cs
+++ ChatButlerProjectB/Program.cs
@@ -7,8 +7,42 @@ namespace ChatButlerProjectB
         static void Main(string[] args)
         {
             Butler Winston = new Butler();
+            Register Register = new Register();
 
+            bool loggedin = false;
+            
             Console.WriteLine(Winston.Greet());
+            Console.WriteLine(Winston.ShowComponents());
+            Console.WriteLine("Kies het nummer waar je heen wilt gaan!");
+            string chosenInput = Console.ReadLine();
+
+            if (chosenInput != "1" && chosenInput != "2" && chosenInput != "3")
+            {
+                Console.WriteLine("Dat is een ongeldig nummer");
+            }
+            else if (chosenInput == "1")
+            {
+                Console.WriteLine("Review");
+            }
+            else if (chosenInput == "2")
+            {
+                Console.WriteLine("Reservering");
+            }
+            else if (chosenInput == "3")
+            {
+                if (loggedin)
+                {
+                    Console.WriteLine("Dat is een ongeldig nummer");
+                }
+                else
+                {
+                    Console.WriteLine("Register");
+                }
+            }
+            else if(chosenInput == "ma names cheff")
+            {
+                Console.WriteLine("Cheff");
+            }
         }
     }
 }
