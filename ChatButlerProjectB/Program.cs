b6d13e0aaf65df4574aa9b3e58837c7cd6aabe09
Moesab Ajdid <75260026+Edfreded@users.noreply.github.com>
2021-04-21 15:38:44 +0200

    Update Program.cs
    small update

--- ChatButlerProjectB/Program.cs
+++ ChatButlerProjectB/Program.cs
@@ -1,14 +1,51 @@
-﻿using System;
+using System;
 
 namespace ChatButlerProjectB
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             Butler Winston = new Butler();
+            Register reg = new Register();
+            PlaceReservation res = new PlaceReservation();
 
+            bool loggedin = false;
+            
             Console.WriteLine(Winston.Greet());
+            Console.WriteLine(Winston.ShowComponents());
+            Console.WriteLine("Kies het nummer waar u heen wilt gaan!");
+            string chosenInput = Console.ReadLine();
+
+            if (chosenInput != "1" && chosenInput != "2" && chosenInput != "3" && chosenInput != "4" && chosenInput != "5" && chosenInput != "chef input")
+            {
+                Console.WriteLine("Dat is een ongeldig nummer");
+                Main();
+            }
+            else if(chosenInput == "5")
+            {
+                Console.WriteLine("Bedankt voor uw bezoek");
+            }
+            else if (chosenInput == "1")
+            {
+                Console.WriteLine("Review");
+            }
+            else if (chosenInput == "2")
+            {
+                res.Reservation();
+            }
+            else if (chosenInput == "3")
+            {
+                reg.MainReg();
+            }
+            else if (chosenInput == "4")
+            {
+                Console.WriteLine("Inloggen");
+            }
+            else if(chosenInput == "ma names cheff")
+            {
+                Console.WriteLine("Cheff");
+            }
         }
     }
 }
