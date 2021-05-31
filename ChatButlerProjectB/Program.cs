f4c377ebb8e7e09f5c874ce13cbbcac5d9fb8942
rob <1006286@hr.nl>
2021-05-31 14:13:05 +0200

    []

--- ChatButlerProjectB/Program.cs
+++ ChatButlerProjectB/Program.cs
@@ -1,12 +1,12 @@
 using System;
-using Newtonsoft.Json;
-using ChatButlerProjectB;
-using System.Collections.Generic;
-using System.Globalization;
-using Newtonsoft.Json.Converters;
-using System.IO;
-using System.Threading;
-using System.Text.RegularExpressions;
+//using Newtonsoft.Json;
+//using ChatButlerProjectB;
+//using System.Collections.Generic;
+//using System.Globalization;
+//using Newtonsoft.Json.Converters;
+//using System.IO;
+//using System.Threading;
+//using System.Text.RegularExpressions;
 
 namespace ChatButlerProjectB
 {
@@ -15,9 +15,10 @@ namespace ChatButlerProjectB
         public static void Main()
         {
             Console.Clear();
-            Butler Winston = new Butler();
 
             //bool loggedin = false;
+
+            Butler Winston = new Butler();
 
             Winston.Log(3, "Menu");
             Console.WriteLine(Winston.Greet());
@@ -39,6 +40,7 @@ namespace ChatButlerProjectB
        
         public static void MenuNotLoggedIn(string i)
         {
+            Butler Winston = new Butler();
             Register reg = new Register();
             Login log = new Login();
             Review rev = new Review();
@@ -47,37 +49,46 @@ namespace ChatButlerProjectB
 
             if (i == "1")
             {
+                Winston.Log(2, "Invoer: <1>");
                 rev.Get_reviews();
             }
             else if(i == "2")
             {
+                Winston.Log(2, "Invoer: <2>");
                 reg.MainReg();
             }
             else if(i == "3")
             {
+                Winston.Log(2, "Invoer: <3>");
                 log.MainLogin();
             }
             else if(i == "4")
             {
+                Winston.Log(2, "Invoer: <4>");
                 smember.MainSearch();
             }
             else if(i == "5")
             {
+                Winston.Log(2, "Invoer: <5>");
                 Console.WriteLine("Bedankt voor uw bezoek");
             }
             else if (i == "sesame")
             {
+                Winston.Log(2, "Invoer: <sesame>");
                 chef.MainChef();
             }
             else
             {
+                Winston.Log(2, $"Invoer: <{i}>");
                 Console.WriteLine("Dat is een ongeldig nummer");
+                
                 Main();
             }
         }
 
         public static void MenuLoggedIn(string i)
         {
+            Butler Winston = new Butler();
             Login log = new Login();
             Account acc = new Account();
             Review rev = new Review();
@@ -88,47 +99,58 @@ namespace ChatButlerProjectB
 
             if (i == "1")
             {
+                Winston.Log(2, "Invoer: <1>");
                 rev.Get_reviews();
             }
             else if (i == "2")
             {
+                Winston.Log(2, "Invoer: <2>");
                 rev.Make_review();
                 Program.Main();
             }
             else if (i == "3")
             {
+                Winston.Log(2, "Invoer: <3>");
                 res.Reservation();
             }
             else if (i == "9")
             {
+                Winston.Log(2, "Invoer: <9>");
                 can.DeleteReservation();
             }
             else if (i == "4")
             {
+                Winston.Log(2, "Invoer: <4>");
                 acc.MainAcc();
             }
             else if (i == "5")
             {
+                Winston.Log(2, "Invoer: <5>");
                 smember.MainSearch();
             }
             else if (i == "6")
             {
+                Winston.Log(2, "Invoer: <6>");
                 log.LogUserOut();
             }
             else if (i == "7")
             {
+                Winston.Log(2, "Invoer: <7>");
                 acc.RemoveAccount();
             }
             else if (i == "8")
             {
+                Winston.Log(2, "Invoer: <8>");
                 Console.WriteLine("Bedankt voor uw bezoek");
             }
             else if (i == "sesame")
             {
+                Winston.Log(2, "Invoer: <sesame>");
                 chef.MainChef();
             }
             else
             {
+                Winston.Log(2, $"Invoer: <{i}>");
                 Console.WriteLine("Dat is een ongeldig nummer");
                 Main();
             }
